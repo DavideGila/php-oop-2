799a75a1618a78d456238f42e8b46f0bbad997e9
DavideGila <davide.gila@gmail.com>
2023-12-05 18:43:30 +0100

    exported fetchAll function and made a foreach to print all the cards

--- book.php
+++ book.php
@@ -1,12 +1,16 @@
 <?php
 include __DIR__ . "/Views/header.php";
+include __DIR__ ."/Model/Book.php";
+$books = Book::fetchAll();
 ?>
 
 <section class="container">
     <h2>Books</h2>
     <div class="row">
         <?php
-        
+            foreach ($books as $book) {
+                $book->printCard();
+            }
         ?>
     </div>
 </section>
