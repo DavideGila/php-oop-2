77fc8d90632249568c121b99200c30503898d5e3
DavideGila <davide.gila@gmail.com>
2023-12-05 15:59:36 +0100

    created book.php

---
+++ book.php
@@ -0,0 +1,16 @@
+<?php
+include __DIR__ . "/Views/header.php";
+?>
+
+<section class="container">
+    <h2>Books</h2>
+    <div class="row">
+        <?php
+        
+        ?>
+    </div>
+</section>
+
+<?php
+include __DIR__ . "/Views/footer.php";
+?>
