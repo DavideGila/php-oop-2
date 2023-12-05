9fbb8f1ed81bf42934f8344a30e8365592f8d0f0
DavideGila <davide.gila@gmail.com>
2023-12-05 16:47:38 +0100

    imported fetchAll and printed all cards

--- game.php
+++ game.php
@@ -1,12 +1,16 @@
 <?php
 include __DIR__ . "/Views/header.php";
+include __DIR__ ."/Model/Game.php";
+$games = Game::fetchAll();
 ?>
 
 <section class="container">
     <h2>Games</h2>
     <div class="row">
         <?php
-
+            foreach ($games as $game) {
+                $game->printCard();
+            }
         ?>
     </div>
 </section>
