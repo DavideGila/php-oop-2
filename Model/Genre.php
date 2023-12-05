7ebf8746cff8df930727462d7b5c93ecb19b3107
DavideGila <davide.gila@gmail.com>
2023-12-05 15:20:43 +0100

    created static function fetchAll

--- Model/Genre.php
+++ Model/Genre.php
@@ -1,11 +1,29 @@
 <?php
 class Genre
 {
-    private string $genre;
+    private string $name;
 
-    function __construct($genre)
+    function __construct($name)
     {
-        $this->genre = $genre;
+        $this->name = $name;
+    }
+    public function drawGenre()
+    {
+        return "<span class='badge text-bg-primary'>$this->name</span>";
+    }
+    public static function fetchAll()
+    {
+
+        $genreString = file_get_contents(__DIR__ . '/genre_db.json');
+        $genreList = json_decode($genreString, true);
+
+        $genres = [];
+
+        foreach ($genreList as $item) {
+            $genres[] = new Genre($item);
+        }
+        return $genres;
     }
 }
+
 ?>
