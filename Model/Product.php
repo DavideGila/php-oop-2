fa4fff596b65e4e9c0460485966173ffe1d66311
DavideGila <davide.gila@gmail.com>
2023-12-06 19:05:07 +0100

    created an exception for sale

--- Model/Product.php
+++ Model/Product.php
@@ -5,10 +5,24 @@ class Product
     use DrawCard;
     public float $price;
     public int $quantity;
+    private int $sale = 0;
 
     function __construct($price, $quantity){
         $this->price = $price;
         $this->quantity = $quantity;
     }       
+
+    public function setSale($percentage) {
+        if ($percentage < 5 || $percentage > 100) {
+            throw new Exception('Your perecentage is out of range.');
+        } else {
+            $this->sale = $percentage;
+        }
+    }
+
+    public function getSale()
+    {
+        return $this->sale;
+    }
 }
 ?>
